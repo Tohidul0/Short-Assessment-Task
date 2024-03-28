4867287b69b04f252a7509096cfd004a20263356
Tohidul0 <tohidulalam967418@gmail.com>
2024-03-29 00:53:24 +0600

    mongodb add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,18 @@
-import express from "express";
+import express from  "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.MONGO)
+.then( ()=>{
+    console.log("mongodb is connected"); 
+})
+.catch((err)=>{
+    console.log(err);
+})
 
 
 app.listen(3000 , () =>{
